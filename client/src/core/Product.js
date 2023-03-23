d67bb6089da4c817eca31cba8896be3f899d16f1
Meeth <mjdavda02@gmail.com>
2023-03-23 23:39:33 +0530

    gui commit

--- client/src/core/Product.js
+++ client/src/core/Product.js
@@ -1,77 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import Layout from './Layout';
-import { read, listRelated } from './apiCore';
-import Card from './Card';
-import ProductReviews from './Review';
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { read, listRelated } from "./apiCore";
+import Card from "./Card";
+import ProductReviews from "./Review";
 
 const Product = (props) => {
-	const [product, setProduct] = useState({});
-	const [relatedProduct, setRelatedProduct] = useState([]);
-	const [error, setError] = useState(false);
+  const [product, setProduct] = useState({});
+  const [relatedProduct, setRelatedProduct] = useState([]);
+  const [error, setError] = useState(false);
 
-	const loadSingleProduct = (productId) => {
-		read(productId).then((data) => {
-			if (data?.error) {
-				setError(data?.error);
-			} else {
-				setProduct(data);
-				console.log(data);
-				// fetch related products
-				// listRelated(data._id).then((data) => {
-				//   if (data?.error) {
-				//     setError(data?.error);
-				//   } else {
-				//     setRelatedProduct(data);
-				//   }
-				// });
-			}
-		});
-	};
+  const loadSingleProduct = (productId) => {
+    read(productId).then((data) => {
+      if (data?.error) {
+        setError(data?.error);
+      } else {
+        setProduct(data);
+        console.log(data);
+        // fetch related products
+        // listRelated(data._id).then((data) => {
+        //   if (data?.error) {
+        //     setError(data?.error);
+        //   } else {
+        //     setRelatedProduct(data);
+        //   }
+        // });
+      }
+    });
+  };
 
-	useEffect(() => {
-		const productId = props.match.params.productId;
-		loadSingleProduct(productId);
-	}, [props]);
+  useEffect(() => {
+    const productId = props.match.params.productId;
+    loadSingleProduct(productId);
+  }, [props]);
 
-	return (
-		<Layout
-			title={product && product.name}
-			description={
-				product &&
-				product.description &&
-				product.description.substring(0, 100)
-			}
-			className='container-fluid'
-		>
-			<div className='row'>
-				<div className='col-md-2'></div>
-				<div className='col-md-4 col-sm-12'>
-					<>
-						<h4>Product Details</h4>
-						{product && product.description && (
-							<div>
-								<Card
-									product={product}
-									showViewProductButton={false}
-								/>
-							</div>
-						)}
-					</>
-				</div>
-				<ProductReviews productId={props.match.params.productId} />
+  return (
+    <Layout
+      title={product && product.name}
+      description={
+        product && product.description && product.description.substring(0, 100)
+      }
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-4 col-sm-12">
+          <>
+            <h4>Product Details</h4>
+            {product && product.description && (
+              <div>
+                <Card product={product} showViewProductButton={false} />
+              </div>
+            )}
+          </>
+        </div>
+        <ProductReviews productId={props.match.params.productId} />
 
-				<div className='col-md-4'>
-					{/* <h4>Related products</h4> */}
-					{/* {relatedProduct.map((p, i) => (
+        <div className="col-md-4">
+          {/* <h4>Related products</h4> */}
+          {/* {relatedProduct.map((p, i) => (
             <div className='mb-3' key={i}>
               <Card product={p} />
             </div>
           ))} */}
-				</div>
-				<div className='col-md-2'></div>
-			</div>
-		</Layout>
-	);
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Product;
---
+++ models/foreCasting.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const foreCastingSchema = new mongoose.Schema({
+  sales: Number,
+});
